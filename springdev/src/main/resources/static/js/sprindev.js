c1a778b84836c9e2b288a938333ea245f8d3eda5
JAgus12 <gonzalezjagustin42@gmail.com>
2025-05-09 11:51:33 +0200

    cambios

--- springdev/src/main/resources/static/js/sprindev.js
+++
@@ -1,8 +0,0 @@
-$iniciosesion=document.querySelector("a")
-console.log($iniciosesion)
-
-
-$iniciosesion.addEventListener("click", (event) => {
-    event.preventDefault()
-    console.log("¡Has hecho clic!");
-});
