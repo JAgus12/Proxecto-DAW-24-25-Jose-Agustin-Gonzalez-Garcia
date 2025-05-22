2b9c6014e42bb652b1cf07b2394ac87f0087e00b
JAgus12 <gonzalezjagustin42@gmail.com>
2025-05-22 16:37:11 +0200

    menu

--- springdev/src/main/resources/static/js/login.js
+++ springdev/src/main/resources/static/js/login.js
@@ -1,9 +1,24 @@
 const $d=document,
       $inicio=$d.querySelector("button")
-console.log($inicio)
+      $ver=$d.querySelector("i")
+      $password=$d.querySelector("input[type='password']")
+
+
+// console.log($inicio)
+// console.log($ver)
+// console.log($password)
 
 $inicio.addEventListener("click",ev=>{
     ev.preventDefault()
     console.log("click")
     window.location.href='/src/main/resources/static/principal.html'
 })
+
+$ver.addEventListener("click",ev=>{
+    if($password.type=='text'){
+        $password.type='password'
+    }else{
+        $password.type='text'
+    }
+})
+
