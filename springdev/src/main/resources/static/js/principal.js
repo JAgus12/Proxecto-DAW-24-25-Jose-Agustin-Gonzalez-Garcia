d9c54127e9dcea0171133fd879c7cfd6724455db
JAgus12 <gonzalezjagustin42@gmail.com>
2025-05-27 16:26:45 +0200

    frontend

--- springdev/src/main/resources/static/js/principal.js
+++ springdev/src/main/resources/static/js/principal.js
@@ -1,10 +1,21 @@
 const $d=document,
       $menu=$d.querySelector(".izquierda").querySelector("i")
-      $sidebar=$d.querySelector(".sidebar")
+      $sidebar=$d.querySelector(".sidebar").querySelectorAll("a")
+      $botonCompartir=$d.querySelector(".compartir")
+      $salir=$d.querySelector(".derecha").querySelector("a")
 
+console.log($salir)
 console.log($menu)
 console.log($sidebar)
 
 $menu.addEventListener("click",ev=>{
     $sidebar.classList.toggle('menu-toggle')
+}) 
+
+$salir.addEventListener("click",ev=>{
+    window.location.href="../login.html"
+})
+
+$sidebar.addEventListener("click",ev=>{
+
 })
