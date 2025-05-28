7d0ebf4bd62c087da19893d22745ba908a9e1259
JAgus12 <gonzalezjagustin42@gmail.com>
2025-05-28 20:05:38 +0200

    frontend

--- springdev/src/main/resources/static/js/principal.js
+++ springdev/src/main/resources/static/js/principal.js
@@ -1,13 +1,40 @@
 const $d=document,
       $menu=$d.querySelector(".izquierda").querySelector("i")
       $sidebar=$d.querySelector(".sidebar")
+      $navmisTareas=$d.querySelector("#tareas")
+      $misTareas=$d.querySelector(".tareas")
+      $navCrearTarea=$d.querySelector("#crearTarea")
+      $crearTarea=$d.querySelector(".crearTarea")
+      $navCompartidos=$d.querySelector("#compartir")
+      $compartidos=$d.querySelector(".compartidos")
+      $navCuenta=$d.querySelector("#cuenta")
+      $cuenta=$d.querySelector(".cuenta")
       $botonCompartir=$d.querySelector(".compartir")
       $salir=$d.querySelector(".derecha").querySelector("a")
-      $misTareas=$d.querySelector("")
+      $main=$d.querySelector("main")
+    
+    
 
-console.log($salir)
-console.log($menu)
-console.log($sidebar)
+     
+const secciones=[$misTareas,$crearTarea,$compartidos,$cuenta]
+
+function mostrarSeccion(seccion) {
+    secciones.forEach(el=>el.classList.remove("activa"))
+    seccion.classList.add("activa")
+}
+//console.log($salir)
+//console.log($menu)
+//console.log($sidebar)
+// console.log($misTareas)
+// console.log($crearTarea)
+// console.log($compartidos)
+// console.log($cuenta)
+// console.log(secciones)
+// console.log($navmisTareas)
+// console.log($navCrearTarea)
+// console.log($navCompartidos)
+// console.log($navCuenta)
+console.log($main)
 
 $menu.addEventListener("click",ev=>{
     $sidebar.classList.toggle('menu-toggle')
@@ -17,9 +44,22 @@ $salir.addEventListener("click",ev=>{
     window.location.href="../login.html"
 })
 
-$sidebar.addEventListener("click",ev=>{
-    if(ev.target.tagName=="I"||"A"){
-        console.log("has hecho click en ali")
-        console.log(ev.target.dataset.seccion)
-    }
+$navmisTareas.addEventListener("click",ev=>{
+    document.body.style.backgroundColor=" rgb(245, 245, 245)"
+    mostrarSeccion($misTareas)
+})
+
+$navCrearTarea.addEventListener("click",ev=>{
+    document.body.style.backgroundColor=" rgb(245, 245, 245)"
+    mostrarSeccion($crearTarea)
+})
+
+$navCompartidos.addEventListener("click",ev=>{
+    document.body.style.backgroundColor=" rgb(245, 245, 245)"
+    mostrarSeccion($compartidos)
+})
+
+$navCuenta.addEventListener("click",ev=>{
+    mostrarSeccion($cuenta)
+    document.body.style.backgroundColor="black"
 })
