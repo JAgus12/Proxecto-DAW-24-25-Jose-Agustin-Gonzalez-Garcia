e1b71ca8ad7f449194491cb6363c0c871c833e1c
JAgus12 <gonzalezjagustin42@gmail.com>
2025-05-29 16:30:42 +0200

    jwt

--- springdev/src/main/resources/static/js/login.js
+++ springdev/src/main/resources/static/js/login.js
@@ -1,17 +1,55 @@
 const $d=document,
-      $inicio=$d.querySelector("button")
+      $inicioSesion=$d.querySelector("button")
       $ver=$d.querySelector("i")
       $password=$d.querySelector("input[type='password']")
+      $usuario=$d.querySelector("input[name='usuario']")
 
 
 // console.log($inicio)
 // console.log($ver)
 // console.log($password)
+console.log($usuario)
 
-$inicio.addEventListener("click",ev=>{
+const urlLogin="http://localhost:8080/auth/login"
+
+function ajax(options) {
+    const {url,method,fExito,fError,data}=options
+
+    fetch(url,{
+        method:method||"GET",
+        headers:{
+            "Content-type":"application/json; charset=utf-8"
+        },
+        body:JSON.stringify(data)
+    })
+    .then(resp=>resp.ok?resp.json():Promise.reject(resp))
+    .then(json=>fExito(json))
+    .catch(error=>fError(error))
+}
+
+
+
+
+function comprobarLogin(password,usuario) {
+    ajax({
+        url:urlLogin,
+        method:"POST",
+        fExito:json=>{
+            console.log(json)
+        },
+        fError:error=>console.log(error),
+        data:{
+            usuario:usuario,
+            password:password
+        }
+    })
+}
+
+$inicioSesion.addEventListener("click",ev=>{
     ev.preventDefault()
-    console.log("click")
-    window.location.href='../principal.html'
+    comprobarLogin()
+    //console.log("click")
+    //window.location.href='../principal.html'
 })
 
 $ver.addEventListener("click",ev=>{
